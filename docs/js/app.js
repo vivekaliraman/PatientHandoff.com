62f09df9ae3d0ee6326e163d261d82fc733c22a7
Vivek Kaliraman <vivek.kaliraman@gmail.com>
2017-10-16 20:21:31 -0700

    First Draft of Caringly/Patient handoff Website in Foundation Framework

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,1 +1,12 @@
 $(document).foundation()
+// closes the panel on click outside
+$(document).mouseup(function (e) {
+  var container = $('#contact-panel');
+  if (!container.is(e.target) // if the target of the click isn't the container...
+  && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+      container.removeClass('is-active');
+    }
+});
+
+
